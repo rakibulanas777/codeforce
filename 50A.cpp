c71175a1961ea7b0d588ebaa7c911866fa41bf7d
unknown <rakibulanas033@gmail.com>
2022-11-16 23:25:54 +0600

    solve 50A solution

---
+++ 50A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int M, N;
+    cin >> M >> N;
+    int mul = M * N;
+    int ans;
+    ans = mul / 2;
+    cout << ans << endl;
+    return 0;
+}
