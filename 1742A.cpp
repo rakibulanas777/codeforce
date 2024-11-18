1b271afb6013429b39931e95503626f3ef3ce42c
rakibulanas3935 <rakibulanas033@gmail.com>
2024-11-18 10:14:44 +0600

    accept

---
+++ 1742A.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+
+    int a;
+    int b;
+    int c;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a >> b >> c;
+        if ((a + b == c) || (a + c == b) || (b + c == a))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
